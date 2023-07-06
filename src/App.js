36087e024056bf50144513decf0307c5d3efdf4e
Ranjit Chavan <61783587+ranjitpchavan@users.noreply.github.com>
2023-07-06 15:18:02 +0530

    Files added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ContactForm from './components/Contact-Form';
+import "./App.css"
+
+const App = () => {
+  return (
+    <div className='centerForm'>
+      <h1>Contact Form</h1>
+      <ContactForm />
+    </div>
+  );
+};
+
+export default App;
